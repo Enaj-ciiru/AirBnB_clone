8c49e4514e8f30fc458871f5a9615cecf4ee2802
WaltTersly <terslykifugo96@gmail.com>
2022-08-10 04:47:14 +0300

    base_model2

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,27 @@ from datetime import datetime
 class BaseModel:
     """base_model that defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init method for BaseModel class
 
         Args: 
             id : string - assign with an uuid when an instance is created
             created_at : datetime - assign with the current datetime when an instance is created
 		    updated_at : datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
-        """        
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """  
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def __str__(self):
         """str method for BaseModel class
